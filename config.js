31e3cad79686ebeacb47fc1f42b1b92e438d053d
nqhung253 <148047056+nqhung253@users.noreply.github.com>
2023-10-20 20:11:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Các cậu';
+const giftUrl = 'https://www.instagram.com/stories/highlights/17996928440243829/';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng các cậu';
+const contentCard = 'Chúc các cậu có một ngày 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với các cậu trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
